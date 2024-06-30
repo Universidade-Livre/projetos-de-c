58fd88c5328b61e7ee0441572984a087e592fe34
Júnior <b.sep@live.com>
2024-06-30 15:06:48 -0300

    atualização do exercicio pitagoras

--- c/pitagoras.c
+++ c/pitagoras.c
@@ -3,31 +3,38 @@
 
 float square_area(float size);
 float hypotenuse(float height, float base);
-float hypotenuse_square_area(float hypotenuse);
 float triangle_area(float height, float base);
 
-// Dado um triângulo com altura 6.0 e base 8.0, calcule os seguintes valores:
-// Área do quadrado da altura
-// Área do quadrado da base
-// Hipotenusa
-// Área do quadrado da hipotenusa
-// Área do triângulo
+/*
+ Dado dois triângulos com alturas 8.0 e 6.0 e base 6.0 e 12.0, implemente funções que calculam:
+ O valor da hipotenusa desses triângulos
+ O valor da área do quadrado da hipotenusa
+ O valor da área do quadrado da altura do triângulo
+ O valor da área do quadrado da base do triângulo
+ O valor da área do triângulo
+
+ Fórmulas:
+    Teorema de pitágoras: a² = b² + c²
+    Área de um triângulo: (altura * base) / 2
+*/
 Test(Aritmetica, pitagoras)
 {
-    float height = 6.0;
-    float base = 8.0;
+    float height = 8.0;
+    float base = 6.0;
+    float height2 = 12.0;
+    float base2= 10.0;
 
-    cr_expect(epsilon_eq(flt, square_area(height), 36, 1), "Área do quadrado da altura deve ser 36");
-    cr_expect(epsilon_eq(flt, square_area(base), 64, 1), "Área do quadrado da base deve ser 64");
-    cr_expect(epsilon_eq(flt, hypotenuse(height, base), 10, 1), "Hipotenusa deve ter valor de 10");
-    cr_expect(epsilon_eq(flt, hypotenuse_square_area(hypotenuse(height, base)), 100, 1), "Quadrado da hipotenusa deve ter valor de 100");
-    cr_expect(epsilon_eq(flt, triangle_area(height, base), 24, 1), "Área do triângulo deve ser 24");
+    cr_expect(ieee_ulp_eq(flt, hypotenuse(height, base), 10, 4));
+    cr_expect(ieee_ulp_eq(flt, square_area(hypotenuse(height, base)), 100, 4));
+    cr_expect(ieee_ulp_eq(flt, square_area(height), 36, 4));
+    cr_expect(ieee_ulp_eq(flt, square_area(base), 64, 4));
+    cr_expect(ieee_ulp_eq(flt, triangle_area(height, base), 24, 4));
 
-    cr_expect(epsilon_eq(flt, square_area(height), 36, 1), "Área do quadrado da altura deve ser 36");
-    cr_expect(epsilon_eq(flt, square_area(base), 64, 1), "Área do quadrado da base deve ser 64");
-    cr_expect(epsilon_eq(flt, hypotenuse(height, base), 10, 1), "Hipotenusa deve ter valor de 10");
-    cr_expect(epsilon_eq(flt, hypotenuse_square_area(hypotenuse(height, base)), 100, 1), "Quadrado da hipotenusa deve ter valor de 100");
-    cr_expect(epsilon_eq(flt, triangle_area(height, base), 24, 1), "Área do triângulo deve ser 24");
+    cr_expect(ieee_ulp_eq(flt, hypotenuse(height2, base2), 15.620499351813308, 4));
+    cr_expect(ieee_ulp_eq(flt, square_area(hypotenuse(height2, base2)), 244, 4));
+    cr_expect(ieee_ulp_eq(flt, square_area(height2), 100, 4));
+    cr_expect(ieee_ulp_eq(flt, square_area(base2), 144, 4));
+    cr_expect(ieee_ulp_eq(flt, triangle_area(height2, base2), 60, 4));
 }
 
 float square_area(float size)
@@ -38,13 +45,6 @@ float square_area(float size)
 }
 
 float hypotenuse(float height, float base)
-{
-    // implementação ...
-
-    return 0.0;
-}
-
-float hypotenuse_square_area(float hypotenuse)
 {
     // implementação ...
 
